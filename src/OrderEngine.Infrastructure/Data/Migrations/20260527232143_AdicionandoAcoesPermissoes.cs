6acfc240bb9a2903fda93b08771c9ec1acf6ee3b
kaiquefreire05 <kaiquefreiresantos05@gmail.com>
2026-05-30 15:26:28 -0300

    feat: Add migration for ActionPermissions and update .gitignore with common exclusions

---
+++ src/OrderEngine.Infrastructure/Data/Migrations/20260527232143_AdicionandoAcoesPermissoes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderEngine.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoAcoesPermissoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
